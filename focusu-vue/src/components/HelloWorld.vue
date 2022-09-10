3be8c16990c9bccc79f74d64ff42162d451eb108
Mohamed Habarneh <92408806+MohamedHabarneh@users.noreply.github.com>
2022-09-10 15:06:33 -0700

    vue and navbar setup
    Created the structure for our vue project using vue cli. Also setup the basic stuff for the navbar.

---
+++ focusu-vue/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <div class="navbar">
+        <div class="container">
+            <a class="logo" href="#">FocusU</a>
+            <img id="mobile-cta" class="mobile-menu" src="../assets/menu.svg" alt="open menu"> 
+            <nav class="nav-list">
+                <img id="mobile-exit" class="mobile-menu-exit"  src="../assets/exit.svg" alt="exit menu">
+                <br>
+                <ul class="primary-nav">
+                    <li class="current"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Tips</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../styles.css"
+</style>
