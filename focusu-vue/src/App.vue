c45545fc34b1acbbefcdefe798baf47b6e280cf4
Mohamed Habarneh <92408806+MohamedHabarneh@users.noreply.github.com>
2022-09-19 18:25:45 -0700

    pomodoro template setup
    Will be changed with the pomodoro, short, and long button along with the start/stop buttons.

--- focusu-vue/src/App.vue
+++ focusu-vue/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <NavBar />
+  <PomodoroSetup/>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import PomodoroSetup from './components/PomodoroSetup.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar
-  }
+    NavBar,
+    PomodoroSetup
+}
 }
 </script>
 
