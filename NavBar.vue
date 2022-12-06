2866fda7ab372de0f1683246b3083fcb35887537
Kevin Le <55897802+bunnyman156@users.noreply.github.com>
2022-12-06 02:21:22 -0800

    modal changes

---
+++ NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <Modal @close ="toggleModal" :modalActive = "modalActive">
+  <div class="modal-content">
+    <h1> About </h1>
+    <p> FocusU was designed as a way to combine the Pomodoro Technique with the lofi-study aesthetic to help students get in the zone. </p>
+  </div>
+</Modal>
+
+  <div class="app">
+    <div class="navbar">
+        <div class="container">
+            <a class="logo" href="#">FocusU</a>
+            <img id="mobile-cta" class="mobile-menu" src="../assets/menu.svg" alt="open menu"> 
+            <nav class="nav-list">
+                <img id="mobile-exit" class="mobile-menu-exit"  src="../assets/exit.svg" alt="exit menu">
+                <br>
+                <ul class="primary-nav">
+                    <li class="current"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <button @click ="toggleModal" type = "button">Open Modal</button>
+                    <!-- <li><a href="#">Tips</a></li> -->S
+                </ul>
+            </nav>
+        </div>
+        
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from "@/components/TempModal.vue";
+import { ref } from 'vue'
+export default {
+  name: "NavBar",
+  components: {
+    Modal,
+  },
+
+  setup() {
+    const modalActive = ref(false);
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value; 
+    }
+    return { modalActive, toggleModal };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../styles.css"
+</style>
---
+++ TempModal.vue
@@ -0,0 +1,65 @@
+<template>
+
+<transition name = "modal-animation">
+    <div v-show ="modalActive" class = "modal">
+        <transition name = "modal-animation-inner">
+            <div v-show ="modalActive" class="modal-inner">
+                <i @click = "close" class = "far fa-times-circle"></i>
+                <!-- Modal Content -->
+                <slot />
+                <button @click = "close" type = "button">Close</button>
+            </div>
+        </transition>
+    </div>
+</transition>
+
+
+</template>
+
+
+
+<script>
+export default {
+    props: ["modalActive"],
+    setup(props, {emit}) {
+        const close = () => {
+            emit("close");
+        }
+        return {close};
+    },
+};
+
+
+</script>
+
+<style>
+
+
+/*
+.modal {
+    display: none;
+    position: fixed;
+    padding-top: 100px;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0, 0, 0, 0.4);
+}
+*/
+
+.modal-inner{
+    display: grid;
+    position: fixed;
+    margin: auto;
+    width: 100vw;
+    height: 100vh;
+    place-items: center;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+
+</style>
