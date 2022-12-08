28df56887648f39e241888a0d7c282774229f780
Mohamed Habarneh <92408806+MohamedHabarneh@users.noreply.github.com>
2022-12-07 22:28:47 -0800

    updated modal template

--- focusu-vue/src/components/NavBar.vue
+++ focusu-vue/src/components/NavBar.vue
@@ -1,17 +1,17 @@
 <template>
+  <Modal v-show="modalActive" @close-modal="(modalActive = false)"/>
   <div class="app">
     <div class="navbar">
         <div class="container">
-            <a class="logo" href="#">{{logoName}}</a>
+            <a class="logo" href="#">FocusU</a>
             <img id="mobile-cta" class="mobile-menu" src="../assets/menu.svg" alt="open menu"> 
             <nav class="nav-list">
                 <img id="mobile-exit" class="mobile-menu-exit"  src="../assets/exit.svg" alt="exit menu">
                 <br>
                 <ul class="primary-nav">
-                    <li class="current"><a href="#">{{homeNav}}</a></li>
-                    <li><a href="#">{{aboutNav}}</a></li>
                     <li class="current"><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="#" @click="toggleModal">About</a></li>
+                    <!-- <button @click ="toggleModal" type = "button">Open Modal</button> -->
                     <!-- <li><a href="#">Tips</a></li> -->
                 </ul>
             </nav>
@@ -22,18 +22,22 @@
 </template>
 
 <script>
+import Modal from "@/components/TempModal.vue";
+import { ref } from 'vue'
+export default {
+  name: "NavBar",
+  components: {
+    Modal,
+  },
 
-import {ref} from 'vue';
-
-export default{
-  setup(){
-    let logoName = ref("FocusU");
-    let homeNav = ref("Home");
-    let aboutNav = ref("About");
-    return {logoName,homeNav,aboutNav};
+  setup() {
+    const modalActive = ref(false);
+    const toggleModal = () => {
+      modalActive.value = !modalActive.value; 
+    }
+    return { modalActive, toggleModal };
   }
 };
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ focusu-vue/src/components/TempModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class = "modal-overlay">
+        <div class="modal" >
+            <h1> About </h1>
+            <p> FocusU was designed as a way to combine the Pomodoro Technique with the lofi-study aesthetic to help students get in the zone. </p>
+            <p> The Pomodoro Technique is a study method which utilizes a 25 minute timer followed by a 5 minute break.</p>
+        </div>
+        <div class="close" @click="$emit('close-modal')">
+            <img class="close-img" src="../assets/exit.svg" alt=""/>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    setup() {
+    },
+};
+
+
+</script>
+
+<style>
+
+
+</style>
